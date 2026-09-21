1ebe0632f1db8a869587752b8b2d3d0bee87bdb5
NicolAlexandra028 <nicol.barragan-a@mail.escuelaing.edu.co>
2026-09-20 20:32:29 -0500

    prueba

--- SlotMachine.java
+++ SlotMachine.java
@@ -61,11 +61,11 @@ public class SlotMachine{
      * @param n Numero de ruedas y simbolos dados por el usuario
      */
     public SlotMachine(int n){
+        wheels = new ArrayList<Wheel>();
         if (n<= 0){
             ok = false; 
             return;
         }
-        wheels = new ArrayList<Wheel>();
         box = new Rectangle();
         box.changeSize(200, 210);
         box.setPosition(70,15);
